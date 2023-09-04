a287608856f7a21439f9bddb594c22977784c829
sistemas <ariecripers@gmail.com>
2023-09-04 16:12:41 -0300

    version 04-09

--- app/src/main/java/cl/santos/animales/MainActivity.java
+++ app/src/main/java/cl/santos/animales/MainActivity.java
@@ -1,8 +1,10 @@
 package cl.santos.animales;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Encuentra el ImageButton con ID "otro"
+        ImageButton botonOtro = findViewById(R.id.ibOtro);
+
+        // Configura un OnClickListener para el ImageButton
+        botonOtro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para iniciar la actividad DatosOtroActivity
+                Intent intent = new Intent(MainActivity.this, DatosOtro.class);
+                startActivity(intent);
+            }
+        });
     }
 }
